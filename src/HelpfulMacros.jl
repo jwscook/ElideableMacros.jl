83446b4c04c4a3e1ed1e7cd52ca163999376d5a9
James Cook <cookjws@gmail.com>
2020-01-20 21:21:54 +0000

    they work?

--- src/HelpfulMacros.jl
+++ src/HelpfulMacros.jl
@@ -1,6 +1,7 @@
-module HelpfulMacros
+module Macros
 
 export @elidableassert, @elidablenanzeroer
+
 
 """
 The Julia @assert macro, except it can be compiled by one of two ways:
@@ -10,7 +11,7 @@ or
 but not both
 """
 macro elidableassert(assertion, messages...)
-  elide = haskey(ENV, "ELIDE_ASSERTS") &&
+  local elide = haskey(ENV, "ELIDE_ASSERTS") &&
     ENV["ELIDE_ASSERTS"] ∈ ("yes", "true", "1", "on");
   elide |= @isdefined(elideasserts) ? elideasserts() : false;
   if @isdefined(elideasserts) && haskey(ENV, "ELIDE_ASSERTS") &&
@@ -18,49 +19,50 @@ macro elidableassert(assertion, messages...)
     error("Set only ENV[\"ELIDE_ASSERTS\"] or elideasserts(), not both.");
   end
   if !elide
-    esc(:(
-      if !isempty($messages)
-        @assert $assertion $messages
+    quote
+      if !isempty($(esc(messages)))
+        @assert $(esc(assertion)) $(esc(messages))
       else
-        @assert $assertion
+        @assert $(esc(assertion))
       end
-      ))
+    end
   end
 end
 
 """
 Replace NaNs with zeros
 Compile out this macro by one of two ways:
-1) Set `ENV["ELIDE_NAN_ZEROER"]` to one of `"yes"`, `"true"`, `"1"`, or `"on"`
+1) Set `ENV["ELIDE_NANZEROER"]` to one of `"yes"`, `"true"`, `"1"`, or `"on"`
 or
 2) Put the function `elidenanzeroer() = false` in the module that calls @elidablenanzeroer
 but not both
 """
 macro elidablenanzeroer(value)
-  elide = haskey(ENV, "ELIDE_NAN_ZEROER") &&
-    ENV["ELIDE_NAN_ZEROER"] ∈ ("yes", "true", "1", "on");
+  local elide = haskey(ENV, "ELIDE_NANZEROER") &&
+    ENV["ELIDE_NANZEROER"] ∈ ("yes", "true", "1", "on");
   elide |= @isdefined(elidenanzeroer) ? elidenanzeroer() : false;
-  if @isdefined(elidenanzeroer) && haskey(ENV, "ELIDE_NAN_ZEROER") &&
-    ENV["ELIDE_NAN_ZEROER"] ∈ ("no", "false", "0", "off");
-    error("Set only ENV[\"ELIDE_NAN_ZEROER\"] or elidenanzeroer(), not both.");
+  if @isdefined(elidenanzeroer) && haskey(ENV, "ELIDE_NANZEROER") &&
+    ENV["ELIDE_NANZEROER"] ∈ ("no", "false", "0", "off");
+    error("Set only ENV[\"ELIDE_NANZEROER\"] or elidenanzeroer(), not both.");
   end;
   if !elide
-    esc(:(
-      _replace(x::T) where {T<:Real} = isnan(x) ? zero(T) : x;
-      function _replace(x::T) where {T<:Complex};
-      local r = real(x);
-      local i = imag(x);
-      local br = isnan(r);
-      local bi = isnan(i);
+    return quote
+      @inline _replace(x::T) where {T<:Real} = ifelse(isnan(x), zero(T), x);
+      @inline function _replace(x::T) where {T<:Complex};
+      r, i = reim(x);
+      br = isnan(r);
+      bi = isnan(i);
       (!br && !bi) && return x;
       (!br && bi) && return T(r, 0);
       (br && !bi) && return T(0, i);
       (br && bi) && return T(0, 0);
       end;
-      return _replace.($value)
-      ))
+      _replace.($(esc(value)))
+    end
   else
-    esc(:(return $value))
+    return quote $(esc(value)) end
   end
 end
+
+
 end
